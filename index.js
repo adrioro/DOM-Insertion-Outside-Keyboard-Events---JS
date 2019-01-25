e7e897bf62e3ef8fc51aac4bbe87911e3eabfdb6
adrioro <artistsvillage@me.com>
2019-01-25 11:22:05 -0500

    Third

--- index.js
+++ index.js
@@ -1,7 +1,12 @@
 //.keydown
-//On keydown move text to box.
+//On keydup move text to div box.
 
 $("#textarea").keyup(function () {
-    var text = $('textarea#textarea').val(); //Retrieving text from textarea.
-    $("div#boxId").html(text);
+    let text = $("#textarea").val(); //Retrieving text from textarea.
+    $("div#boxId").html(text); //Adding it to the box.
+});
+//On click move text to div.
+$("#button").click(function () { //When button is clicked.
+    let text2 = $("textarea#textarea2").val(); //Text is retrieved.
+    $("#boxId2").html(text2); //And moved to the box.
 });
