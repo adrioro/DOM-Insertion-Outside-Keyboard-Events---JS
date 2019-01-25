43092e6de19d29949e03c300569566bbb99a9089
adrioro <artistsvillage@me.com>
2019-01-25 15:15:26 -0500

    Fourth commit

--- index.js
+++ index.js
@@ -10,3 +10,11 @@ $("#button").click(function () { //When button is clicked.
     let text2 = $("textarea#textarea2").val(); //Text is retrieved.
     $("#boxId2").html(text2); //And moved to the box.
 });
+
+//On click move text after paragraph.
+$("#button3").click(function () {
+    let text3 = $("#textarea3").val(); //Retrieving text from textarea.
+    //let mynewText = $("p").html(text3);
+    $("<p></p>").html(text3).insertAfter("p.myParagh"); //Adding it to the box.
+    //$("mynewText").insertAfter("p.myParagh");
+});
